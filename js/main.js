f072dc0a3e84c6d9a3af265709e3e95a8859dfa9
islomNasrullayev <120090847+islomNasrullayev@users.noreply.github.com>
2023-01-15 02:08:18 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const burderMenu = () => {
+  const menu = document.querySelector('#menu')
+  const burger = document.querySelector('.burger')
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    menu.classList.toggle('hidden')
+    menu.classList.toggle('flex')
+  })
+}
+burderMenu()
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+const modeBtn = document.querySelector('#mode')
+modeBtn.addEventListener('click', () => {
+  document.querySelector('html').classList.toggle('dark')
+  if (document.querySelector('html').classList.contains('dark')) {
+    localStorage.theme = 'dark'
+  } else localStorage.theme = 'light'
+})
